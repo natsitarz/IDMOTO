81e32e2dd2444859a09ed4d366d32c9071d4e00e
natsitarz <natsitarz@icloud.com>
2025-06-09 01:22:53 +0200

    fix: Update metadata title and description for improved clarity and SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import PageLoaderWrapper from "@/app/parts/PageLoaderWrapper";
 import { Watermark } from "@/app/parts/watermark";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -16,8 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "IDMOTO | Log In",
-  description: "Show the world your dream car",
+  title: "IDMOTO",
+  description: "Show off your ride!",
 };
 
 export default function RootLayout({
@@ -27,6 +28,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="keywords"
+          content="car profile, car social network, vehicle history, automotive, car community, your car profile, car enthusiasts, car management, car sharing, vehicle profile, car lovers, automotive enthusiasts, car showcase, car collection"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="IDMOTO – Show off your ride!" />
+        <meta
+          property="og:description"
+          content="Create and share your car profile with IDMOTO."
+        />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
