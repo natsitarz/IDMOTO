ca1b9817da30ccc4e7f2e37bd1f209eb2129f9c1
natsitarz <natsitarz@icloud.com>
2025-08-21 12:59:04 +0200

    style: Update background to a gradient in CarPageInner component

--- src/app/parts/CarPageInner.tsx
+++ src/app/parts/CarPageInner.tsx
@@ -455,7 +455,7 @@ export default function CarPageInner() {
   }
 
   return (
-    <div className="relative min-h-[calc(100dvh-67px)] bg-zinc-900 flex flex-col items-center font-[family-name:var(--font-geist-sans)] px-4">
+    <div className="relative min-h-[calc(100dvh-67px)] bg-gradient-to-br from-zinc-950 via-zinc-900 to-zinc-950 flex flex-col items-center font-[family-name:var(--font-geist-sans)] px-4">
       {/* Hero Section */}
       <div className="animate-fade-in-opacity relative w-full h-96 max-h-[420px] flex items-end justify-start overflow-hidden rounded-b-3xl shadow-xl mb-4">
         <Image
